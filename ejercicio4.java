5a1d61822b7c9bafdb274f86d9ca11b9a997df2b
mik3Blu3 <enanin12992@gmail.com>
2021-06-09 21:58:05 -0600

    Agregue el ejercicio4 en lenguaje Java

---
+++ ejercicio4.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class ejercicio4
+{
+    static Scanner entrada = new Scanner(System.in);
+    public static void main(String args[])
+    {
+        int edadMayor = 0;
+        int edadMenor = 0;
+        int i = 1;
+        int n = 5;
+        System.out.println("Bienvenido al sistema de edad mayor o menor");
+        while(i <= n)
+        {
+            System.out.println("Escriba la edad #"+i);
+            int edad = entrada.nextInt();
+            if(i == 1)
+            {
+                edadMayor =edad;
+                edadMenor = edad;
+            }else if(edad > edadMayor){
+                edadMayor = edad;
+            }if(edad < edadMenor){
+                edadMenor = edad;
+            }
+            i++;
+        }
+        System.out.println("La edad mayor es: "+edadMayor);
+        System.out.println("La edad menor es: "+edadMenor);
+    }
+}
